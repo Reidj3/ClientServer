2de26ad4de2a4adabe24a143cbb05be0811d25d1
Reidj3 <sp1.ke@mail.ru>
2020-12-05 20:54:24 +0300

    Сервер отправляет сообщение

--- src/ru/reidj/clientserver/Server.java
+++ src/ru/reidj/clientserver/Server.java
@@ -1,13 +1,22 @@
 package ru.reidj.clientserver;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(8000);
-        socket.accept();
-        socket.close();
+        ServerSocket serverSocket = new ServerSocket(8000);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("<h1>Hello World</h1>");
+            writer.flush();
+            writer.close();
+
+            clientSocket.close();
+        }
     }
 }
