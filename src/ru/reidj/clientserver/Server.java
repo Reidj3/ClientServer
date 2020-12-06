85a3d92b01a7fa743a543c00f5165d1a7a9c36e2
Reidj3 <sp1.ke@mail.ru>
2020-12-06 21:02:20 +0300

    +Клиент

--- src/ru/reidj/clientserver/Server.java
+++ src/ru/reidj/clientserver/Server.java
@@ -9,7 +9,6 @@ public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        while (true) {
             Socket clientSocket = serverSocket.accept();
             OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
             writer.write("<h1>Hello World</h1>");
@@ -17,6 +16,5 @@ public class Server {
             writer.close();
 
             clientSocket.close();
-        }
     }
 }
