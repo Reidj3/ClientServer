7e801b5702abe241d693c8767b2416e772ae8278
Reidj3 <sp1.ke@mail.ru>
2020-12-07 15:30:45 +0300

    Обмен сообщениями

--- src/ru/reidj/clientserver/Server.java
+++ src/ru/reidj/clientserver/Server.java
@@ -1,20 +1,29 @@
 package ru.reidj.clientserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8000);
-
+        ServerSocket serverSocket = new ServerSocket(8200);
+        while (true) {
             Socket clientSocket = serverSocket.accept();
-            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-            writer.write("<h1>Hello World</h1>");
-            writer.flush();
-            writer.close();
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+
+            System.out.println(reader.readLine());
+
+            writer.write("Сообщение принято");
+            writer.flush();
+
+            reader.close();
+            writer.close();
             clientSocket.close();
+        }
     }
 }
--- src/ru/reidj/clientserver/client/Client.java
+++ src/ru/reidj/clientserver/client/Client.java
@@ -5,11 +5,18 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-            Socket clientSocket = new Socket("127.0.0.1", 8000);
+        Socket clientSocket = new Socket("127.0.0.1", 8200);
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println(reader.readLine());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
 
-            clientSocket.close();
+        writer.write("Отправляю сообщение на сервер\n");
+        writer.flush();
+
+        System.out.println(reader.readLine());
+
+        reader.close();
+        writer.close();
+        clientSocket.close();
     }
 }
