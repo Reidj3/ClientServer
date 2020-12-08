6619cab857534d048812480d36b0c9432c549bed
Reidj3 <sp1.ke@mail.ru>
2020-12-08 22:53:45 +0300

    Чат

--- src/ru/reidj/clientserver/Server.java
+++ src/ru/reidj/clientserver/Server.java
@@ -3,7 +3,6 @@ package ru.reidj.clientserver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,15 +13,10 @@ public class Server {
             Socket clientSocket = serverSocket.accept();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
 
             System.out.println(reader.readLine());
 
-            writer.write("Сообщение принято");
-            writer.flush();
-
             reader.close();
-            writer.close();
             clientSocket.close();
         }
     }
--- src/ru/reidj/clientserver/client/Client.java
+++ src/ru/reidj/clientserver/client/Client.java
@@ -1,22 +1,52 @@
 package ru.reidj.clientserver.client;
 
-import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class Client {
-    public static void main(String[] args) throws IOException {
+public class Client extends JFrame {
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldInput = new JTextField();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new Client();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private Client() throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 8200);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
 
-        writer.write("Отправляю сообщение на сервер\n");
-        writer.flush();
+        log.setEditable(false);
+        log.setLineWrap(true);
 
-        System.out.println(reader.readLine());
+        add(log, BorderLayout.CENTER);
+        fieldInput.addActionListener(e -> {
+            try {
+                OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+                writer.write(fieldInput.getText());
+                writer.flush();
+                log.append(fieldInput.getText() + "\n");
+                fieldInput.setText(null);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
 
-        reader.close();
-        writer.close();
-        clientSocket.close();
+        add(fieldInput, BorderLayout.SOUTH);
+
+        setVisible(true);
     }
 }
+
